89a8f55015c29957a6987a2029e3575320bf9f95
DESKTOP-3T85H9P\Adrian <csi23-awanuer@colegioaltair.net>
2024-03-12 14:24:16 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio8repasoStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ejercicio6repasoStrings { 
+    class program {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Escribe una cadena de texto");
+            string cadena = Console.ReadLine();
+            Console.WriteLine("¿Que caracter desea cambiar?");
+            string caracterACambiar = Console.ReadLine();
+            Console.WriteLine("¿Por cual lo desea cambiar?");
+            string caracterSutituidor = Console.ReadLine();
+            Console.WriteLine(cadena.Replace(caracterACambiar, caracterSutituidor));
+
+        }
+    } 
+}
